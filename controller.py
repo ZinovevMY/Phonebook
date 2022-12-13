0dba405709f5c1bae092e924909b4c002d6ed583
Mikhail Zinovev <mehan@list.ru>
2022-12-13 17:41:20 +0300

    plus step

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ def click():
     view.main_menu()
 
 
-def op_choice():
+def get_op_choice():
     while True:
         op = input('-> ')
         if not (op in '1234560'):
@@ -13,3 +13,8 @@ def op_choice():
             continue
         return op
         break
+
+
+def get_search_data():
+    obj = input('Введите фамилию или номер абонента (если ничего не вводить, будет выдан весь справочник): ')
+    return obj
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import controller as contr
 
 if __name__ == '__main__':
     contr.click()
-    op = contr.op_choice()
+    op = contr.get_op_choice()
     print(op)
