07a2d403448715c4336088bcd913abcd8370f523
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-12-15 20:46:11 +0300

    add function

--- data_op.py
+++ data_op.py
@@ -5,16 +5,20 @@
 #  - Удаление элемента
 def output_data(data: list) -> None:  # output is list of contacts
     if len(data) > 0:
-        print("-"*105)
+        print("-"*105+'\n')
         for contact in data:
             while len(contact) < 5:
                 contact.append('-')
             print(contact[0].center(20), '|'.center(1), contact[1].center(20), '|'.center(1),
                   contact[2].center(20), '|'.center(
-                      1), contact[3].center(15), '|'.center(1), contact[4].center(16), '|'.center(1))
-            print("-"*105)
+                      1), contact[3].center(15), '|'.center(1), contact[4].center(16), '|'.center(1) + '\n')
+            print("-"*105+'\n')
     else:
         print('PhoneBook is empty')
 
 
-def find_contact()
+output_data([['qwew', '12312'], ['wqeqwrr', '213']])
+
+
+def find_contact():
+    pass
